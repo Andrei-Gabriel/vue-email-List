e4651b8c32336c9f2e3d27fc4fb65236810038e9
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-12-10 10:10:42 +0100

    Correzione esercizio

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const app = new Vue({
     el: "#root",
     data: {
-        prova: null,
+        emails: [],
     },
     methods: {
     },
     created() {
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((response) => {
-            this.prova = response.data.response;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        for(let i = 0; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+                this.emails.push(response.data.response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
     }
 });
